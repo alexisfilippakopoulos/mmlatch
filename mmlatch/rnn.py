5d4600d5dc315c018fd7d9cdc138e0357fba89a4
Evangelos Xaniadakis <echaniadakis@gmail.com>
2025-06-07 20:29:44 +0300

    fix

--- mmlatch/rnn.py
+++ mmlatch/rnn.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 from mmlatch.attention import Attention
+from mmlatch.memory import MemoryModule
 from mmlatch.util import pad_mask
 
 
